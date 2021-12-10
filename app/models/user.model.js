626344d4df8776ba574eb53313ad299deeb05fad
PierreMjn <pierre.mijon@edu.ece.fr>
2021-12-10 14:29:58 +0100

    Unique username delete to email unique and adding firstname and lastname

--- app/models/user.model.js
+++ app/models/user.model.js
@@ -1,6 +1,9 @@
 module.exports = (sequelize, Sequelize) => {
     const User = sequelize.define("users", {
-        username: {
+        firstname: {
+            type: Sequelize.STRING
+        },
+        lastname: {
             type: Sequelize.STRING
         },
         email: {
